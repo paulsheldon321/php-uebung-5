19c706ed70ec1d33e1b31fdd8f0e825ed55931a8
siriglueck <siriluck.rattanapong@gmail.com>
2025-11-20 10:53:04 +0000

    README aktualisiert

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Übung 5 von Siri
+# Übung 5 von Siri 
+### Aufgabe:
+ * ✅ 0) Einer von Euch legt ein öffentliches Git-Repo an von wo aus Ihr alle pushen und pullen könnt (optional aber hilfreich)
+ * ✅ 1) Erstelle die Struktur: data/, inc/, class/, public/
+ * ✅ 2) Lade Notizen aus data/notes.json und zeige sie in public/index.php.
+ * 🟡 3) Implementiere add.php und delete.php (POST) Hinweis: verstecktes Formularfeld.
+ * 🟡 4) index.php (Grundgerüst wie hier in dieser Datei):
+ * ✅ - Ausgabe der Notizen (wenn leeres Array: Info ausgeben)
+ * ✅ - Formular zum Hinzufügen neuer Notizen
+ * 🟡    - Formular zum Löschen der entsprechenden Notiz (<input type="hidden">)
+ * 🟡 5) inc/tools.php
+ * 🟡 - Funktionen zum Laden, Speichern und Laden der Notizen
+ * ✅ 6) add.php
+ * ✅ - Funktionalität zum Hinzufügen neuer Notizen
+ * ✅ 7) delete.php
+ * ✅ - Funtionalität zum Löschen einer Notiz
+ * ✅ 8) class/Note.php
+ * ✅ - Klasse für Notizen
+ 
--- public/index.php
+++ public/index.php
@@ -4,25 +4,7 @@ declare(strict_types=1);
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 
-/**
- * Aufgabe:
- * ✅ 0) Einer von Euch legt ein öffentliches Git-Repo an von wo aus Ihr alle pushen und pullen könnt (optional aber hilfreich)
- * ✅ 1) Erstelle die Struktur: data/, inc/, class/, public/
- * ✅ 2) Lade Notizen aus data/notes.json und zeige sie in public/index.php.
- * 🟡 3) Implementiere add.php und delete.php (POST) Hinweis: verstecktes Formularfeld.
- * 🟡 4) index.php (Grundgerüst wie hier in dieser Datei):
- * ✅ - Ausgabe der Notizen (wenn leeres Array: Info ausgeben)
- * ✅ - Formular zum Hinzufügen neuer Notizen
- *    - Formular zum Löschen der entsprechenden Notiz (<input type="hidden">)
- * 🟡 5) inc/tools.php
- *    - Funktionen zum Laden, Speichern und Laden der Notizen
- * ✅ 6) add.php
- *    - Funktionalität zum Hinzufügen neuer Notizen
- * 🟡 7) delete.php
- *    - Funtionalität zum Löschen einer Notiz
- * ✅ 8) class/Note.php
- *    - Klasse für Notizen
- */
+
 
 require_once dirname(__DIR__) . '/class/Note.php';
 require_once dirname(__DIR__) . '/inc/tools.php';
